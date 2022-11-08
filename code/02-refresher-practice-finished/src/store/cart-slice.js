ae976b78bc8fb8c8b6aea8aac581a0392edd822a
Furqan Masood <55999845+furki911@users.noreply.github.com>
2022-11-08 21:53:05 +0530

    Adding Post & Get Request

--- code/02-refresher-practice-finished/src/store/cart-slice.js
+++ code/02-refresher-practice-finished/src/store/cart-slice.js
@@ -3,14 +3,20 @@ import { createSlice } from '@reduxjs/toolkit';
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
+    changed: false,
     items: [],
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -19,6 +25,7 @@ const cartSlice = createSlice({
           totalPrice: newItem.price,
           name: newItem.title
         });
+
       } else {
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
@@ -28,10 +35,12 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
